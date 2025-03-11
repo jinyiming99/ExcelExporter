1e592565717389022877443a1cabb162f50b374b
jin <yiming.jin@yingxiong.com>
2025-03-11 11:23:05 +0800

    分类

---
+++ ConsoleApp1/ExportToFile/ToCsharpFile/CsharpConfigFile.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1.Config;
+using ConsoleApp1.NewDatas;
+
+namespace ConsoleApp1;
+
+public class CsharpConfigFile
+{
+    public static async Task Export(GlobalConfig config,Dictionary<string,ClassData> dic)
+    {
+        var tasks = new List<Task>();
+        foreach (var v in dic)
+        {
+            Console.WriteLine($"export {v.Key}");
+            tasks.Add(ExportToFileHelper.ExportStructFile(config._outPath,config._nameSpace,v.Value));
+            tasks.Add(ExportToFileHelper.ExportDataFile(config._outPath,config._nameSpace,v.Value));
+        }
+
+        await Task.WhenAll(tasks);
+        await ExportToFileHelper.ExportAllConfigFile(config._outPath,config._nameSpace,dic.Keys.ToList());
+    }
+}
